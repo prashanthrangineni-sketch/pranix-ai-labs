b5b127ade6dce87fea004df5cbf1ba75cb2fee72
prashanthrangineni-sketch <prashanthrangineni@gmail.com>
2026-05-30 06:51:22 +0530

    F.2B: middleware persistent cookieOptions

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
+import { AUTH_COOKIE_OPTIONS } from '@/lib/auth-cookie-options'
 
 const SUPABASE_URL =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
